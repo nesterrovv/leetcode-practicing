8b0f15390c3758c200ba34f0aa81f421f52206c7
nesterrovv <nesterov.iv2018@gmail.com>
2023-02-18 13:08:11 +0300

    added task #226 solution

---
+++ src/task_226/Solution.java
@@ -0,0 +1,17 @@
+package task_226;
+
+public class Solution {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        return root;
+    }
+
+}
---
+++ src/task_226/TreeNode.java
@@ -0,0 +1,21 @@
+package task_226;
+/** Definition for a binary tree node. */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
