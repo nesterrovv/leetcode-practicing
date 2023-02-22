d0fd9f4d16ec9a4afaa09a3ec230feeef3dfa6ed
nesterrovv <nesterov.iv2018@gmail.com>
2023-02-22 15:41:42 +0300

    added task #27 solution

---
+++ src/task_27/Solution.java
@@ -0,0 +1,32 @@
+package task_27;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+
+    public int removeElement(int[] nums, int val) {
+        Arrays.sort(nums);
+        int amountOfDeletions = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                delete(nums, i);
+                amountOfDeletions++;
+                nums[nums.length - 1] = 51;
+            }
+            if (nums[i] == val) {
+                i--;
+            }
+        }
+        return nums.length - amountOfDeletions;
+    }
+
+    public void delete(int[] array, int index) {
+        for (int i = index; i < array.length-1; i++) {
+            array[i] = array[i+1];
+        }
+    }
+
+}
